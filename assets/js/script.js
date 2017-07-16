88cee6b55dec9dcc6b1a9d1bfe21ba3b672a2647
tim8on <tim8on@gmail.com>
2017-07-16 07:53:10 -0700

    Initialize
    Barely the skeleton of a website. Something to build off of.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// fancyBox
+$(document).ready(function() {
+  $(".various").fancybox({
+    maxWidth: 800,
+    maxHeight: 450,
+    fitToView: false,
+    width: '70%',
+    height: '70%',
+    autoSize: false,
+    closeClick: false,
+    openEffect: 'none',
+    closeEffect: 'none'
+  });
+});
